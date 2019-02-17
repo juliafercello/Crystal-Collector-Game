b4133ef511031fa936086c6523f67bb9675240b2
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-02-16 21:29:08 -0600

    added design and remaining functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 //Main game object
 var crystalCollector = {
     wins: 0,
@@ -10,13 +9,14 @@ var crystalCollector = {
     numMinRange: 19,
     numMaxRange: 120,
 
+    //returns a random number between the provided min and max parameters
     generateRandomNumber: function (minNum, maxNum) {
         var randomNumber = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
         return randomNumber;
     },
 
+    //for each item in the gem class, generate a random number between 1 and 12 and assign to its data-gemvalue attribute.
     assignNumberToGems: function () {
-        //for each item in the gem class, generate a random number between 1 and 12 and assign to its data-gemvalue attribute.
         $(".gem").each(function (index) {
             var gemValue = crystalCollector.generateRandomNumber(crystalCollector.gemMinRange, crystalCollector.gemMaxRange);
             $(this).attr("data-gemValue", gemValue);
@@ -25,88 +25,57 @@ var crystalCollector = {
     },
 
     //pick a random number between 19 and 120 and display on the page 
-    assignNumberToMatch: function() {
-        crystalCollector.numberToMatch = crystalCollector.generateRandomNumber(crystalCollector.numMinRange,  crystalCollector.numMaxRange);
-        $("#numToMatch").text( crystalCollector.numberToMatch); 
+    assignNumberToMatch: function () {
+        crystalCollector.numberToMatch = crystalCollector.generateRandomNumber(crystalCollector.numMinRange, crystalCollector.numMaxRange);
+        $("#numToMatch").text(crystalCollector.numberToMatch);
     },
 
-    updateScore: function(pointsToAdd) {
-        crystalCollector.score += pointsToAdd; 
-        $("#sumOfGems").text(crystalCollector.score); 
+    //Each click on a gem, update the score based on the gem's value (sent as a parameter)
+    updateScore: function (pointsToAdd) {
+        crystalCollector.score += pointsToAdd;
+        $("#sumOfGems").text(crystalCollector.score);
     },
 
-    checkScore: function() { 
+    //Each click on a gem, check to see if score is the number to match or is greater than number to match
+    checkScore: function () {
+        var isRoundOver = false;
+        //if user wins the round, increase score and display on page
         if (crystalCollector.score === crystalCollector.numberToMatch) {
-            alert("you win");
+            crystalCollector.wins++;
+            isRoundOver = true;
         }
+
+        //if user loses the round, increase losses and display on page
         else if (parseInt(crystalCollector.score) > parseInt(crystalCollector.numberToMatch)) {
-            alert("you lose");
-        }; 
+            crystalCollector.losses++;
+            isRoundOver = true;
+        };
+
+        //if the round is over, reset the gems and number to match
+        if (isRoundOver === true) {
+           crystalCollector.startNewRound(); 
+        };
+    },
+
+    startNewRound: function(){
+        crystalCollector.assignNumberToGems();
+        crystalCollector.assignNumberToMatch();
+        crystalCollector.score = 0;
+        $("#numOfWins").text("Wins: " + crystalCollector.wins);
+        $("#numOfLosses").text("Losses: " + crystalCollector.losses);
+        $("#sumOfGems").text(crystalCollector.score);
     }
-
 }
-//Reset Score to Zero
-//resetScore: function () {
-//    this.score = 0;
-// document.getElementById("userScore").innerHTML = wordGuess.score;
-//Reset Tries to 12
-//resetTries: function () {
-//   this.tries = 12;
-//  document.getElementById("tries").innerHTML = wordGuess.tries;
-// },
 
-//Run the Game!!
+//Play the Game!!
 $(document).ready(function () {
 
     crystalCollector.assignNumberToGems();
-    crystalCollector.assignNumberToMatch(); 
-    //crystalCollector.updateScore(3); 
+    crystalCollector.assignNumberToMatch();
 
-    $(".gem").on("click", function() {
-        var belle = parseInt($(this).attr("data-gemValue")); 
-        crystalCollector.updateScore(belle); 
-        crystalCollector.checkScore(); 
-    }); 
-
+    $(".gem").on("click", function () {
+        var gemPoints = parseInt($(this).attr("data-gemValue"));
+        crystalCollector.updateScore(gemPoints);
+        crystalCollector.checkScore();
+    });
 });
-
-//Determine if the letter is in the word
-//document.onkeyup = function (event) {
-//    var keyPress = event.key;
- //   if (wordToGuess.includes(keyPress)) {
-        //then loop through the blanks and replace with the letter in the correct spot(s)
-   //     for (var i = 0; i < wordToGuess.length; i++) {
-     //       if (keyPress === wordToGuess.charAt(i)) {
-       //         wordGuess.rightLetters[i] = wordToGuess.charAt(i);
-         //       wordGuess.showWord();
-           // }
-//         }
-//         //check to see if they won
-//         var stillGuessing = wordGuess.rightLetters.includes("_ ");
-//         if (stillGuessing === false) {
-//             //show the matching animal
-//             var modalTitleWin = "Great Guess!!"
-//             wordGuess.animalImageSource(modalTitleWin);
-//             //increase score and reset with a new word.
-//             wordGuess.increaseScore();
-//             wordGuess.resetTries();
-//             wordGuess.resetLetters();
-//             wordToGuess = wordGuess.currentWord();
-//             wordGuess.prepGuesses();
-//         }
-//     }
-//     else { //no match
-//         wordGuess.addGuessedLetter(keyPress.toUpperCase());
-//         //check to see if they lost and if so, restart the game.
-//         if (wordGuess.tries === 0) {
-//             var modalTitleLose = "GAME OVER!!"
-//             wordGuess.animalImageSource(modalTitleLose);
-//             wordGuess.resetScore();
-//             wordGuess.resetTries();
-//             wordGuess.resetLetters();
-//             wordToGuess = wordGuess.currentWord();
-//             wordGuess.prepGuesses();
-//         }
-//     }
-
-// }
